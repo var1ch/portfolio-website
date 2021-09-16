b6a537d371517b542a822591411c142729d7c0da
Vitalii <varich96@gmail.com>
2021-09-16 10:55:58 +0300

    Updates

--- src/view/sections/content/index.ts
+++
@@ -1,6 +0,0 @@
-import Projects from "./Projects";
-import About from "./About";
-import Skills from "./Skills";
-import Contact from "./Contact";
-
-export { About, Contact, Projects, Skills };
--- src/view/sections/index.ts
+++
@@ -1,4 +0,0 @@
-import Header from "./Header";
-import Navigation from "./Navigation";
-import Footer from "./Footer";
-export { Header, Navigation, Footer };
